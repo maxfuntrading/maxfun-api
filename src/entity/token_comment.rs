050bdd16d6cb68eaef4c6eaf2c34a9853cfddf4c
luckygopher <mr.arvin.hou@gmail.com>
2025-01-21 11:04:51 +0800

    optimize comment api

--- src/entity/token_comment.rs
+++ src/entity/token_comment.rs
@@ -12,6 +12,19 @@ pub struct Model {
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(
+        belongs_to = "super::user::Entity",
+        from = "Column::UserAddress",
+        to = "super::user::Column::Address"
+    )]
+    User,
+}
 
-impl ActiveModelBehavior for ActiveModel {} 
+impl ActiveModelBehavior for ActiveModel {}
+
+impl Related<super::user::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::User.def()
+    }
+} 
