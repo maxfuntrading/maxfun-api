f2ae5da89f0fd7d36ac92d8a3ab507683722bd40
luckygopher <mr.arvin.hou@gmail.com>
2025-02-11 15:56:21 +0800

    filter ranking token_address is empty

--- src/api/ranking/logic.rs
+++ src/api/ranking/logic.rs
@@ -3,13 +3,14 @@ use crate::core::AppState;
 use crate::entity::{token_info, token_summary};
 use crate::utility::{with_domain, LibResult};
 use chrono::Utc;
-use sea_orm::{EntityTrait, JoinType, QueryOrder, QuerySelect, RelationTrait};
+use sea_orm::{EntityTrait, JoinType, QueryOrder, QuerySelect, RelationTrait, ColumnTrait, Condition};
 
 const TOP_LIMIT: u64 = 10;
 
 pub async fn get_process_ranking(app_state: AppState) -> LibResult<schema::RankingResp> {
     let tokens = token_info::Entity::find()
         .find_also_related(token_summary::Entity)
+        .filter(token_info::Column::TokenAddress.ne(""))
         .order_by_desc(token_summary::Column::BondingCurve)
         .limit(TOP_LIMIT)
         .all(&app_state.db_pool)
@@ -25,6 +26,7 @@ pub async fn get_process_ranking(app_state: AppState) -> LibResult<schema::Ranki
 pub async fn get_gainer_ranking(app_state: AppState) -> LibResult<schema::RankingResp> {
     let tokens = token_info::Entity::find()
         .find_also_related(token_summary::Entity)
+        .filter(token_info::Column::TokenAddress.ne(""))
         .order_by_desc(token_summary::Column::PriceRate24h)
         .limit(TOP_LIMIT)
         .all(&app_state.db_pool)
@@ -40,6 +42,7 @@ pub async fn get_gainer_ranking(app_state: AppState) -> LibResult<schema::Rankin
 pub async fn get_market_cap_ranking(app_state: AppState) -> LibResult<schema::RankingResp> {
     let tokens = token_info::Entity::find()
         .find_also_related(token_summary::Entity)
+        .filter(token_info::Column::TokenAddress.ne(""))
         .order_by_desc(token_summary::Column::MarketCap)
         .limit(TOP_LIMIT)
         .all(&app_state.db_pool)
@@ -55,6 +58,7 @@ pub async fn get_market_cap_ranking(app_state: AppState) -> LibResult<schema::Ra
 pub async fn get_volume_ranking(app_state: AppState) -> LibResult<schema::RankingResp> {
     let tokens = token_info::Entity::find()
         .find_also_related(token_summary::Entity)
+        .filter(token_info::Column::TokenAddress.ne(""))
         .order_by_desc(token_summary::Column::Volume24h)
         .limit(TOP_LIMIT)
         .all(&app_state.db_pool)
